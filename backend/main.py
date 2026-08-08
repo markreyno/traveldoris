3df42e9033d39b4ac62303194d995c2454838d61
markreyno <116620519+markreyno@users.noreply.github.com>
2026-08-08 07:10:19 -0700

    api endpoint update

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,11 @@
-import fastapi from FastAPI
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Hello from TravelDoris"}
+
+@app.post("/trip")
+def create_trip():
+    return {"message": "Planning a trip"}
